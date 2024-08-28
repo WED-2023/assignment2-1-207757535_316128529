90348dc18ed17b94c4864ac1bb299516f2b273ce
Dvir Zandany <127864652+zandanyd@users.noreply.github.com>
2024-08-28 13:13:13 +0300

    28/08 search

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -29,8 +29,8 @@
                 <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"/>
               </svg>       
             </template>
-            <b-dropdown-item value="time">Preparation Time</b-dropdown-item>
-            <b-dropdown-item value="popularity">Popularity</b-dropdown-item>
+            <b-dropdown-item @click="updateSortOption('time')">Preparation Time</b-dropdown-item>
+            <b-dropdown-item @click="updateSortOption('popularity')">Popularity</b-dropdown-item>
           </b-dropdown>
         </div>
 
@@ -98,12 +98,22 @@ export default {
       return this.$root.store.username;
     }
   },
+  mounted() {
+    // Load saved search results from localStorage
+    const savedResults = localStorage.getItem('savedRecipes');
+    if (savedResults) {
+      this.recipes = JSON.parse(savedResults);
+    }
+  },
   methods: {
     async changeNumOfPresentedRec(num){
       this.numOfRecipes = num;
       this.search();
     },
-  
+    async updateSortOption(option) {
+      this.selectedSortOption = option;
+      this.search(); // Trigger search with the new sort option
+    },
     async search() {
       try {
         const cuisineFilters = this.selectedFilters.cuisines.join(',');
@@ -115,6 +125,9 @@ export default {
           this.recipes = response.data; // Replace old recipes with new ones
           if (this.recipes.length === 0) {
             this.$root.toast("Failed", "can't find recipes");
+          } else {
+            // Save results to localStorage
+            localStorage.setItem('savedRecipes', JSON.stringify(this.recipes));
           }
         } else {
           this.$root.toast("Failed", "can't find recipes");
